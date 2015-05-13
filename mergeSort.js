6b28b2686918d50027f2be723c038407d100c86a
Madeline <madelinebagby@yahoo.com>
2015-05-13 11:57:46 -0700

    So ya...the first function wasn't correct...this one better be

--- mergeSort.js
+++ mergeSort.js
@@ -4,20 +4,24 @@
 // from left and right.
 
 exports.merge = function(left, right) {
-	var leftI=0;
-	var rightI=0;
+	var i=0;
+	var j=0;
 	var result=[];
-	while(leftI<left.length || rightI<right.length){
-		if (left[leftI]<= right[rightI]){
-			result.push(left[leftI])
-			leftI++;
-		}
-		else if ((right[rightI]< left[leftI]){
-			result.push(right[rightI])
-			rightI++;
-		})
-	}
-}
+	while(i<left.length || j<right.length){
+		if (i === left.length){
+			result.push(right[j]);
+			j++;
+		}else if (j === right.length || left[i]<= right[j]){
+			result.push(left[i]);
+			i++;
+		}else {
+            result.push(right[j])
+            j++;
+        }
+	} 
+    return result;
+}; 
+
 
 // Merge sort uses the merge function in order to
 // sort an array. Given an array of numbers in any

2dc3347f584c58a552eb00507bfbae3310cd6661
Madeline <madelinebagby@yahoo.com>
2015-05-13 11:36:44 -0700

    edited the files to include more tests and functions

--- mergeSort.js
+++ mergeSort.js
@@ -2,7 +2,21 @@
 // Merge takes two sorted arrays (left, right) and
 // returns one array which contains the sorted numbers
 // from left and right.
+
 exports.merge = function(left, right) {
+	var leftI=0;
+	var rightI=0;
+	var result=[];
+	while(leftI<left.length || rightI<right.length){
+		if (left[leftI]<= right[rightI]){
+			result.push(left[leftI])
+			leftI++;
+		}
+		else if ((right[rightI]< left[leftI]){
+			result.push(right[rightI])
+			rightI++;
+		})
+	}
 }
 
 // Merge sort uses the merge function in order to
--- spec/mergeSortSpec.js
+++ spec/mergeSortSpec.js
@@ -4,12 +4,20 @@ describe('Merge', function() {
   it('should merge two arrays of size 1', function() {
      expect(mergeSort.merge([1],[2])).toEqual([1,2]);
   });
-
 });
 
 describe('Merge Sort', function() {
   it('should sort two arrays of size 1', function() {
-     expect(mergeSort.merge([2],[1])).toEqual([1,2]);
+     expect(mergeSort.mergeSort([2],[1])).toEqual([1,2]);
+  });
+  it('should sort two arrays of size 4', function(){
+  	expect(mergeSort.mergeSort([2,3,1,0],[6,8,4,4])).toEqual([0,1,2,3,4,4,6,8]);
+  });
+  it('should sort three arrays of size 2', function(){
+  	expect(mergeSort.mergeSort([2,3],[0,1],[6,8])).toEqual([0,1,2,3,6,8]);
+  });
+   it('should sort two arrays of different sizes', function(){
+  	expect(mergeSort.mergeSort([2,3,0],[0,1])).toEqual([0,0,1,2,3]);
   });
 });
 
